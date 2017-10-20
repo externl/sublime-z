3126ef5e32429e21e1eb23e71ba1936104b65c15
Joe George <joe@externl.com>
2017-10-20 13:15:19 -0400

    Add new mode and make list default

--- Commands.py
+++ Commands.py
@@ -42,31 +42,43 @@ class ZCommand:
         matches = self.run('--list')
         return re.findall('^[0-9]+\.?[0-9]*\s+(.*)', matches, re.MULTILINE) if matches else None
 
-class ZOpenDirectoryCommand(sublime_plugin.WindowCommand):
+class ZJumpToDirectoryCommand(sublime_plugin.WindowCommand):
     def run(self, menu=None, action=None):
-        self.window.show_input_panel("Z Open Directory RegEx", "", self.on_show_input_panel_done, None, None)
+        self.mode = Settings.get('mode')
+        if self.mode == 'list_all':
+            self.mode_list(ZCommand(''))
+        else:
+            self.window.show_input_panel("Z: Directory RegEx", "", self.on_show_input_panel_done, None, None)
 
     def on_show_input_panel_done(self, directory_regex):
         z = ZCommand(directory_regex)
 
-        if Settings.get('mode') == 'best_match':
-            try:
-                directory = z.getBestMatch()
-                if directory:
-                    self.open_directory(directory)
-                else:
-                    sublime.message_dialog('No match for {}'.format(directory_regex))
-            except Exception as e:
-                sublime.error_message(str(e))
+        if self.mode == 'best_match':
+            self.mode_best_match(z)
+        elif self.mode == 'list':
+            self.mode_list(z)
         else:
-            try:
-                directory_list = z.getList()
-                if directory_list:
-                    self.show_directories(directory_list)
-                else:
-                    sublime.message_dialog('No matches for {}'.format(directory_regex))
-            except Exception as e:
-                sublime.error_message(str(e))
+            raise Exception('Unknown z mode')
+
+    def mode_best_match(self, z):
+        try:
+            directory = z.getBestMatch()
+            if directory:
+                self.open_directory(directory)
+            else:
+                sublime.message_dialog('No match for {}'.format(z.directory_regex))
+        except Exception as e:
+            sublime.error_message(str(e))
+
+    def mode_list(self, z):
+        try:
+            directory_list = z.getList()
+            if directory_list:
+                self.show_directories(directory_list)
+            else:
+                sublime.message_dialog('No matches for {}'.format(z.directory_regex))
+        except Exception as e:
+            sublime.error_message(str(e))
 
     def on_directory_selected(self, directory):
         if directory:
@@ -83,4 +95,4 @@ class ZOpenDirectoryCommand(sublime_plugin.WindowCommand):
 
     def show_directories(self, directory_list):
         selectedCallback = lambda index: self.on_directory_selected(directory_list[index] if index > -1 else None)
-        self.window.show_quick_panel(directory_list, selectedCallback, 0, 0, None)
+        self.window.show_quick_panel(directory_list, selectedCallback, sublime.KEEP_OPEN_ON_FOCUS_LOST, 0, None)
